d03c8577abf0f030ea9e3f9a08d45b5539b86fbe
felixfaisal <faisalahmedfarooq46@gmail.com>
2018-12-09 19:44:37 +0530

    Removed few files

--- Testing-Goodreads-API.py
+++ Testing-Goodreads-API.py
@@ -2,7 +2,10 @@ import requests
 
 def main():
    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"GHWJKEZAp54QUjwwKZWFg", "isbns": "9781632168146"})
-   print(res.json())
+   data = res.json()
+   rate= data["books"]
+   new= rate["isbn"]
+   print(new)
 
 if __name__ == "__main__":
     main()

b3be0983e22dbdb7d465ce0f0ffa1c124778224c
felixfaisal <faisalahmedfarooq46@gmail.com>
2019-04-05 07:48:06 +0530

    Adding password

--- Testing-Goodreads-API.py
+++ Testing-Goodreads-API.py
@@ -4,8 +4,8 @@ def main():
    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":"GHWJKEZAp54QUjwwKZWFg", "isbns": "9781632168146"})
    data = res.json()
    rate= data["books"]
-   new= rate["isbn"]
-   print(new)
+   print(res.text)
+   print(rate)
 
 if __name__ == "__main__":
     main()
